27f880be2e95d0730b16254f53da0ab5b2104588
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-09-17 11:42:07 -0400

    Add an API router

---
+++ backend/routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+router.post('/test', function (req, res) {
+    res.json({ requestBody: req.body });
+});
+
+module.exports = router;
