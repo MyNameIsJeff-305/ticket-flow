f4003ea7d4480c1c5e4fe8afa5f92e405dd72f2f
MyNameIsJeff-305 <michelgarcia950528@icloud.com>
2024-10-02 22:38:56 -0400

    Fixing color transitions in styling

--- frontend/src/components/Splash/Footer.jsx
+++ frontend/src/components/Splash/Footer.jsx
@@ -1,4 +1,5 @@
 import { FaGithub } from "react-icons/fa";
+import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
 
 const Footer = () => {
     return (
@@ -6,7 +7,7 @@ const Footer = () => {
             <div className="footer-links" style={{width:"100%"}}>
                 <a href="https://github.com/MyNameIsJeff-305"><FaGithub /> Contact Me</a>
             </div>
-            <p style={{ fontSize: "10px", textAlign: "right" }}>ticketFlow is an intuitive and powerful ticket management system designed specifically for IT businesses. With streamlined workflows, real-time tracking, and comprehensive reporting features, ticketFlow ensures that IT support teams can efficiently manage tasks, resolve issues faster, and provide exceptional service. From ticket creation to resolution, ticketFlow keeps your IT operations organized, helping you stay on top of all support requests and improve customer satisfaction.</p>
+            <ThemeSwitch />
         </footer>
     );
 };
